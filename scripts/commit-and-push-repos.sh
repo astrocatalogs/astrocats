eb7ee4856fed331feedb106306170feee394e3b1
guillochon <guillochon@gmail.com>
2016-01-14 23:49:54 -0800

    updated commit script

--- scripts/commit-and-push-repos.sh
+++ scripts/commit-and-push-repos.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, exiting"
+	exit
+fi
+
 repos=('sne-pre-1990' 'sne-1990-1999' 'sne-2000-2004' 'sne-2005-2009' 'sne-2010-2014' 'sne-2015-2019')
 cd ..
 for repo in ${repos[@]}; do
 	cd ${repo}
 	pwd
 	git add -u
-	git commit -a -m '$1' && git push
+	git commit -a -m "$1"
+	git push
 	cd ..
 done
