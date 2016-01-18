6e57b5bfde6ea8ee16d6fbc71fca8ed2ac2e2aa9
guillochon <guillochon@gmail.com>
2016-01-17 21:29:11 -0800

    removed explicit repo list from push/pull scripts

--- scripts/commit-and-push-repos.sh
+++ scripts/commit-and-push-repos.sh
@@ -5,12 +5,13 @@ if [ $# -eq 0 ]
 	exit
 fi
 
-repos=('sne-pre-1990' 'sne-1990-1999' 'sne-2000-2004' 'sne-2005-2009' 'sne-2010-2014' 'sne-2015-2019')
+readarray repos < rep-folders.txt
+echo ${repos[*]}
 cd ..
 for repo in ${repos[@]}; do
 	cd ${repo}
 	pwd
-	git add -u
+	git add -A
 	git commit -a -m "$1"
 	git push
 	cd ..
