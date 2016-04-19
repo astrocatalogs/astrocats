48e448524553bafea7035e3f73dc72b0e3fe0311
guillochon <guillochon@gmail.com>
2016-04-19 06:49:04 -0700

    added radio/ray to pull

--- scripts/pull-repos.sh
+++ scripts/pull-repos.sh
@@ -4,6 +4,8 @@ git pull
 repos=($(awk -F= '{print $1}' rep-folders.txt))
 repos+=('sne-internal')
 repos+=('sne-external')
+repos+=('sne-external-radio')
+repos+=('sne-external-xray')
 repos+=('sne-external-spectra')
 repos+=('sne-external-WISEREP')
 echo ${repos[*]}
