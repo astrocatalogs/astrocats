ce9c698a05183892d32f6984ffc466ee035f8549
guillochon <guillochon@gmail.com>
2016-06-03 06:43:25 -0700

    Added script to prune host-imgs.json, useful if large number of coordinates change.

---
+++ scripts/prune-host-json.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.5
+
+import json
+
+with open('../names', 'r') as f:
+    names = f.read().splitlines()
+
+with open('../hostimgs.json', 'r') as f:
+    hostimgs = json.loads(f.read())
+
+count = 0
+newhostimgs = []
+for ei, entry in enumerate(hostimgs):
+    if entry[0] not in names:
+        count = count + 1
+        newhostimgs.append(entry)
+
+print(count)
+
+jsonstring = json.dumps(newhostimgs, indent='\t', separators=(',',':'))
+with open('../hostimgs.json', 'w') as f:
+    f.write(jsonstring)
