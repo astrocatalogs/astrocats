4c6cd167fba13c78bfed0e9b8562a03b641fa43e
lzkelley <lkelley@cfa.harvard.edu>
2016-06-06 12:35:09 -0400

    MAINT: get 'utils' functions working.

--- scripts/utils/__init__.py
+++ scripts/utils/__init__.py
@@ -1,20 +1,22 @@
 """General utility functions used by multiple OSC scripts.
 """
 
+print("utils/__init__.py")
+
 from . import digits
-from digits import *
+from . digits import *
 from . import events
-from events import *
+from . events import *
 from . import photometry
-from photometry import *
+from . photometry import *
 from . import repos
-from repos import *
-from . import tq
-from tq import *
+from . repos import *
+from . import tq_funcs
+from . tq_funcs import *
 
 __all__ = []
 __all__.extend(digits.__all__)
 __all__.extend(events.__all__)
 __all__.extend(photometry.__all__)
 __all__.extend(repos.__all__)
-__all__.extend(tq.__all__)
+__all__.extend(tq_funcs.__all__)
