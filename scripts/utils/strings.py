b0d2c3d0e25e597be3f08e150d3b5cb2de72abdf
lzkelley <lkelley@cfa.harvard.edu>
2016-06-24 13:12:19 -0400

    MAINT: add 'single_spaces' to '__all__' variable for easy import.

--- scripts/utils/strings.py
+++ scripts/utils/strings.py
@@ -1,13 +1,15 @@
 """
 """
 
-__all__ = ['rep_chars']
+__all__ = ['rep_chars', 'single_spaces']
+
 
 def rep_chars(string, chars, rep = ''):
     for c in chars:
         if c in string:
             string = string.replace(c, rep)
     return string
+
 
 def single_spaces(string):
     return ' '.join(list(filter(None, string.split())))
