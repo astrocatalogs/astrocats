f29b0feb00a7fc5a3b933e0a7110f050243ce6d2
guillochon <guillochon@gmail.com>
2016-06-26 15:55:39 -0400

    STY: Linted constants.py

--- scripts/importer/constants.py
+++ scripts/importer/constants.py
@@ -32,7 +32,8 @@ MAX_BANDS = [
     ['R', 'r']        # if not, R-like bands
 ]
 
-PREF_KINDS = ['heliocentric', 'cmb', 'spectroscopic', 'photometric', 'host', 'cluster', '']
+PREF_KINDS = ['heliocentric', 'cmb', 'spectroscopic',
+              'photometric', 'host', 'cluster', '']
 
 NON_SNE_PREFIXES = ['PNVJ', 'PNV J', 'OGLE-2013-NOVA', 'EV*', 'V*', 'Nova']
 
@@ -40,11 +41,13 @@ NON_SNE_PREFIXES = ['PNVJ', 'PNV J', 'OGLE-2013-NOVA', 'EV*', 'V*', 'Nova']
 class TASK:
     name = ''
     nice_name = ''   # Name for pretty printing
-    update = False   # Perform task during update
-    archived = False  # Use archived data ???
+    # Perform task during update
+    update = False
+    # Use archived data ???
+    archived = False
     active = True    # Whether this task should be performed or not
-
-    module = None    # Module in which to find `function` for carrying out this task
+    # Module in which to find `function` for carrying out this task
+    module = None
     function = ''    # Function to execute when carrying out this task
     priority = None  # Order in which tasks should be executed
 
@@ -56,14 +59,16 @@ class TASK:
                 raise ValueError("No attribute '{}'".format(key))
 
     def __repr__(self):
-        retval = ("TASK(name='{}', nice_name='{}', active='{}', update='{}', archived='{}', "
-                  "module='{}', function='{}', priority='{}'")
-        retval = retval.format(self.name, self.nice_name, self.active, self.update, self.archived,
-                               self.module, self.function, self.priority)
+        retval = ("TASK(name='{}', nice_name='{}', active='{}', update='{}', "
+                  "archived='{}', module='{}', function='{}', priority='{}'")
+        retval = retval.format(self.name, self.nice_name, self.active,
+                               self.update, self.archived, self.module,
+                               self.function, self.priority)
         return retval
 
     def current_task(self, args):
-        """Name of current action for progress-bar output, depends on run configuration.
+        """Name of current action for progress-bar output, depends on run
+        configuration.
         """
         ctask = self.nice_name if self.nice_name else self.name
         if args is not None:
@@ -74,14 +79,18 @@ class TASK:
         return ctask
 
     def load_archive(self, args):
-        """Depending on run configuration, whether previously archived data should be loaded.
+        """Depending on run configuration, whether previously archived data
+        should be loaded.
         """
         if not self.archived:
             return False
-        # If we're running in 'archived' mode, and only loading 'archived' things, then True
-        if args.archived and self.name not in args.refresh_list and not args.full_refresh:
+        # If we're running in 'archived' mode, and only loading 'archived'
+        # things, then True
+        if (args.archived and self.name not in args.refresh_list and not
+                args.full_refresh):
             return True
-        # For normal running, if we are not sepcifically refreshing this task, then True
+        # For normal running, if we are not sepcifically refreshing this task,
+        # then True
         if self.name not in args.refresh_list and not args.full_refresh:
             return True
 
