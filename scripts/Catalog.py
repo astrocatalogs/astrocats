79ac8614980833435fec3ee5abc8b02ebb8d889b
guillochon <guillochon@gmail.com>
2016-06-27 14:39:39 -0400

    MAINT: moved logger to Catalog object

--- scripts/Catalog.py
+++ scripts/Catalog.py
@@ -6,7 +6,7 @@ from git import Repo
 
 from scripts import PATH
 
-from .utils import pbar, tprint
+from .utils import get_logger, pbar
 from .importer.funcs import (get_bibauthor_dict, get_biberror_dict,
                              get_extinctions_dict, get_repos_dict)
 
@@ -22,15 +22,17 @@ class Catalog():
     biberror_dict = get_biberror_dict()
     extinctions_dict = get_extinctions_dict()
 
-    def __init__(self, log):
+    log = get_logger()
+
+    def __init__(self):
         all_repos = [self.repos_dict[x] for x in self.repos_dict]
         all_repos = [i for x in all_repos for i in x]
         for repo in pbar(all_repos):
             if not os.path.isdir(PATH.ROOT + "/" + repo):
                 try:
-                    log.warning('Cloning "' + repo +
-                                '" (only needs to be done ' +
-                                'once, may take few minutes per repo).')
+                    self.log.warning('Cloning "' + repo +
+                                     '" (only needs to be done ' +
+                                     'once, may take few minutes per repo).')
                     Repo.clone_from("git@github.com:astrocatalogs/" +
                                     repo + ".git",
                                     PATH.ROOT + "/" + repo)
