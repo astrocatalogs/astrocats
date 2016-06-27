1af0901c2488536aee5077f2ed50ec11d25d52a2
guillochon <guillochon@gmail.com>
2016-06-26 23:58:25 -0400

    MAINT: separated PESSTO to new task

--- scripts/importer/mtasks/general_data.py
+++ scripts/importer/mtasks/general_data.py
@@ -110,32 +110,6 @@ def do_internal(events, stubs, args, tasks, task_obj, log):
     return events
 
 
-def do_pessto(events, stubs, args, tasks, task_obj, log):
-    pessto_path = os.path.join(PATH.REPO_EXTERNAL, 'PESSTO_MPHOT.csv')
-    tsvin = list(csv.reader(open(pessto_path, 'r'), delimiter=','))
-    for ri, row in enumerate(tsvin):
-        if ri == 0:
-            bands = [xx.split('_')[0] for xx in row[3::2]]
-            systems = [xx.split('_')[1].capitalize().replace(
-                'Ab', 'AB') for xx in row[3::2]]
-            continue
-        name = row[1]
-        events, name = Events.add_event(tasks, args, events, name, log)
-        source = events[name].add_source(bibcode='2015A&A...579A..40S')
-        events[name].add_quantity('alias', name, source)
-        for hi, ci in enumerate(range(3, len(row) - 1, 2)):
-            if not row[ci]:
-                continue
-            teles = 'Swift' if systems[hi] == 'Swift' else ''
-            add_photometry(
-                events, name, time=row[2], magnitude=row[
-                    ci], e_magnitude=row[ci + 1],
-                band=bands[hi], system=systems[hi], telescope=teles, source=source)
-
-    events, stubs = Events.journal_events(tasks, args, events, stubs, log)
-    return events
-
-
 def do_scp(events, stubs, args, tasks, task_obj, log):
     current_task = task_obj.current_task(args)
     tsvin = list(csv.reader(
