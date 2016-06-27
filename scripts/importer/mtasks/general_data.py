df8533196162f3ee3eb9885e5ae3c4c477456c2f
guillochon <guillochon@gmail.com>
2016-06-26 23:55:04 -0400

    MAINT: separated ITEP into new task

--- scripts/importer/mtasks/general_data.py
+++ scripts/importer/mtasks/general_data.py
@@ -1,10 +1,8 @@
 """General data import tasks.
 """
 import csv
-import json
 import os
 import re
-import urllib
 from collections import OrderedDict
 from datetime import datetime
 from glob import glob
@@ -109,59 +107,6 @@ def do_internal(events, stubs, args, tasks, task_obj, log):
                                          clean=True, delete=False)
         events.update({new_event.name: new_event})
 
-    return events
-
-
-def do_itep(events, stubs, args, tasks, task_obj, log):
-    current_task = task_obj.current_task(args)
-    itepbadsources = ['2004ApJ...602..571B']
-    needsbib = []
-    with open(os.path.join(PATH.REPO_EXTERNAL, 'itep-refs.txt'), 'r') as refs_file:
-        refrep = refs_file.read().splitlines()
-    refrepf = dict(list(zip(refrep[1::2], refrep[::2])))
-    fname = os.path.join(PATH.REPO_EXTERNAL, 'itep-lc-cat-28dec2015.txt')
-    tsvin = list(csv.reader(open(fname, 'r'),
-                            delimiter='|', skipinitialspace=True))
-    curname = ''
-    for rr, row in enumerate(pbar(tsvin, current_task)):
-        if rr <= 1 or len(row) < 7:
-            continue
-        oldname = 'SN' + row[0].strip()
-        mjd = str(jd_to_mjd(Decimal(row[1].strip())))
-        band = row[2].strip()
-        magnitude = row[3].strip()
-        e_magnitude = row[4].strip()
-        reference = row[6].strip().strip(',')
-
-        if curname != oldname:
-            curname = oldname
-            events, name = Events.add_event(tasks, args, events, oldname, log)
-
-            sec_reference = 'Sternberg Astronomical Institute Supernova Light Curve Catalogue'
-            sec_refurl = 'http://dau.itep.ru/sn/node/72'
-            sec_source = events[name].add_source(
-                srcname=sec_reference, url=sec_refurl, secondary=True)
-            events[name].add_quantity('alias', oldname, sec_source)
-
-            year = re.findall(r'\d+', name)[0]
-            events[name].add_quantity('discoverdate', year, sec_source)
-        if reference in refrepf:
-            bibcode = unescape(refrepf[reference])
-            source = events[name].add_source(bibcode=bibcode)
-        else:
-            needsbib.append(reference)
-            source = events[name].add_source(
-                srcname=reference) if reference else ''
-
-        if bibcode not in itepbadsources:
-            add_photometry(events, name, time=mjd, band=band, magnitude=magnitude,
-                           e_magnitude=e_magnitude, source=sec_source + ',' + source)
-
-    # Write out references that could use aa bibcode
-    needsbib = list(OrderedDict.fromkeys(needsbib))
-    with open('../itep-needsbib.txt', 'w') as bib_file:
-        bib_file.writelines(['%ss\n' % ii for ii in needsbib])
-    events, stubs = Events.journal_events(tasks, args, events, stubs, log)
     return events
 
 
