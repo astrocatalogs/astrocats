0c9aaa11c341fee2d191b0d83417b8f7afa978f8
guillochon <guillochon@gmail.com>
2016-06-26 23:02:28 -0400

    MAINT: separated GRB into new task

---
+++ scripts/importer/mtasks/grb.py
@@ -0,0 +1,40 @@
+"""General data import tasks.
+"""
+import csv
+import os
+
+from scripts import PATH
+from scripts.utils import pbar
+
+from .. import Events
+from ..funcs import load_cached_url
+
+
+def do_grb(events, stubs, args, tasks, task_obj, log):
+    current_task = 'GRB'
+    file_path = os.path.join(PATH.REPO_EXTERNAL, 'GRB-catalog/catalog.csv')
+    csvtxt = load_cached_url(args,
+                             current_task,
+                             ('http://grb.pa.msu.edu/grbcatalog/'
+                              'download_data?cut_0_min=10&cut_0=BAT%20T90'
+                              '&cut_0_max=100000&num_cuts=1&no_date_cut=True'),
+                             file_path)
+    if not csvtxt:
+        return events
+    data = list(csv.reader(csvtxt.splitlines(), delimiter=',',
+                           quotechar='"', skipinitialspace=True))
+    for r, row in enumerate(pbar(data, current_task)):
+        if r == 0:
+            continue
+        (events,
+         name,
+         source) = Events.new_event(tasks, args, events, 'GRB ' +
+                                    row[0], log,
+                                    srcname='Gamma-ray Bursts Catalog',
+                                    url='http://grbcatalog.org')
+        events[name].add_quantity('ra', row[2], source, unit='floatdegrees')
+        events[name].add_quantity('dec', row[3], source, unit='floatdegrees')
+        events[name].add_quantity('redshift', row[8], source)
+
+    events, stubs = Events.journal_events(tasks, args, events, stubs, log)
+    return events
