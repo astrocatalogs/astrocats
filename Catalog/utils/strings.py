34e1e8b80858b6b3ab01d81fbc819dacc77106cd
lzkelley <lkelley@cfa.harvard.edu>
2016-06-29 13:00:01 -0400

    BUG: add new function names to '__all__'.

--- Catalog/utils/strings.py
+++ Catalog/utils/strings.py
@@ -1,7 +1,14 @@
 """
 """
 
-__all__ = ['rep_chars', 'single_spaces', 'get_event_filename']
+from .digits import round_sig
+
+__all__ = ['get_event_filename', 'rep_chars', 'single_spaces',
+           'trim_str_arr', 'uniq_cdl', 'utf8']
+
+
+def get_event_filename(name):
+    return name.replace('/', '_')
 
 
 def rep_chars(string, chars, rep=''):
@@ -15,19 +22,15 @@ def single_spaces(string):
     return ' '.join(list(filter(None, string.split())))
 
 
-def get_event_filename(name):
-    return name.replace('/', '_')
-
-
-def uniq_cdl(values):
-    return ','.join(sorted(list(set(values))))
-
-
 def trim_str_arr(arr, length=10):
     return [str(round_sig(float(x), length)) if
             (len(x) > length and
              len(str(round_sig(float(x), length))) < len(x))
             else x for x in arr]
+
+
+def uniq_cdl(values):
+    return ','.join(sorted(list(set(values))))
 
 
 def utf8(x):
