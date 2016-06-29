437c40302aef0c70c6e6ef410f97c1f2f6be833f
lzkelley <lkelley@cfa.harvard.edu>
2016-06-29 16:10:05 -0400

    MAINT: update imports

--- astrocats/supernovae/supernova.py
+++ astrocats/supernovae/supernova.py
@@ -12,16 +12,16 @@ from astrocats.catalog.entry import KEYS, Entry
 from astrocats.catalog.utils import (alias_priority, bandmetaf, bandrepf,
                                      get_event_filename, get_repo_folders,
                                      get_repo_years, get_sig_digits,
-                                     host_clean, is_number,
+                                     is_number,
                                      jd_to_mjd, make_date_string, pretty_num,
-                                     radec_clean, read_json_dict, same_tag_num,
-                                     same_tag_str, tprint, trim_str_arr,
+                                     read_json_dict,
+                                     tprint, trim_str_arr,
                                      uniq_cdl)
 from cdecimal import Decimal
 
 from .. import FILENAME, PATH, SCHEMA
 from .constants import MAX_BANDS, PREF_KINDS, REPR_BETTER_QUANTITY
-from .utils import (frame_priority, name_clean)
+from .utils import (frame_priority, host_clean, name_clean, same_tag_num, same_tag_str, radec_clean)
 
 
 class SN_KEYS(KEYS):
