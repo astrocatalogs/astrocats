7c336d3a323621be7012a791026e54401bf01c47
lzkelley <lkelley@cfa.harvard.edu>
2016-06-29 14:28:57 -0400

    BUG: add missing 'sorting' and 'strings' submodules.

--- astrocats/catalog/utils/__init__.py
+++ astrocats/catalog/utils/__init__.py
@@ -1,11 +1,12 @@
 """General utility functions used by multiple OSC scripts.
 """
 
-from . import digits, logger, photometry, repos, strings, tq_funcs
+from . import digits, logger, photometry, repos, sorting, strings, tq_funcs
 from .digits import *
 from .logger import *
 from .photometry import *
 from .repos import *
+from .sorting import *
 from .strings import *
 from .tq_funcs import *
 
@@ -14,4 +15,6 @@ __all__.extend(digits.__all__)
 __all__.extend(logger.__all__)
 __all__.extend(photometry.__all__)
 __all__.extend(repos.__all__)
+__all__.extend(sorting.__all__)
+__all__.extend(strings.__all__)
 __all__.extend(tq_funcs.__all__)
