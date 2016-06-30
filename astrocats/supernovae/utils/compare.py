33f809949b3edb80b400eb7300dddf48f04378f5
lzkelley <lkelley@cfa.harvard.edu>
2016-06-30 14:09:16 -0400

    MAINT: rearranged logic to be more understandable.

--- astrocats/supernovae/utils/compare.py
+++ astrocats/supernovae/utils/compare.py
@@ -7,6 +7,7 @@ __all__ = ['same_tag_num', 'same_tag_str']
 
 
 def same_tag_num(photo, val, tag, canbelist=False):
+    '''
     issame = (
         (tag not in photo and not val) or
         (tag in photo and not val) or
@@ -17,7 +18,35 @@ def same_tag_num(photo, val, tag, canbelist=False):
              Decimal(photo[tag]) == Decimal(val)) or
             (isinstance(photo[tag], list) and isinstance(val, list) and
              photo[tag] == val))))))
-    return issame
+    '''
+
+    '''
+    if tag not in photo and not val:
+        return True
+
+    if tag in photo and not val:
+        return True
+    '''
+
+    if not val:
+        return True
+
+    if tag in photo:
+        if not canbelist and Decimal(photo[tag]) == Decimal(val):
+            return True
+
+        if canbelist:
+            if ((isinstance(photo[tag], str) and
+                 isinstance(val, str) and
+                 Decimal(photo[tag]) == Decimal(val))):
+                return True
+
+            if ((isinstance(photo[tag], list) and
+                 isinstance(val, list) and
+                 photo[tag] == val)):
+                return True
+
+    return False
 
 
 def same_tag_str(photo, val, tag):
