da29c4bb2272ceb7dcb06db306570341ddafdeb6
guillochon <guillochon@gmail.com>
2016-06-30 19:59:12 -0400

    ENH: added astrocats sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 from recommonmark.parser import CommonMarkParser
 
 # -- General configuration ------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -11,6 +11,8 @@ Contents:
 .. toctree::
    :maxdepth: 2
 
+.. automodule:: astrocats
+   :members:
 
 
 Indices and tables
