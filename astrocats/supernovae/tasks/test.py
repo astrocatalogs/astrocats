d17af38986dc9f30ccb14f512023c465223dac51
lzkelley <lkelley@cfa.harvard.edu>
2016-06-29 21:23:29 -0400

    ENH: added 'test' task ('test.py').
    test task creates a fake entry, gives it properties, etc.  At each point making sure the results
    look correct.

---
+++ astrocats/supernovae/tasks/test.py
@@ -0,0 +1,166 @@
+"""
+"""
+
+from astrocats.supernovae.supernova import SN_KEYS
+
+TEST_NAME = 'SN-TEST-AA'
+
+FAKE_BIBCODE_1 = '2025Tst...123..456Z'
+FAKE_ALIAS_1 = 'PS-TEST-AB'
+FAKE_REDZ_1 = '1.123'
+
+FAKE_BIBCODE_2 = '1925Tst...987..654A'
+FAKE_ALIAS_2 = 'PTF-TEST-BA'
+FAKE_REDZ_2 = '0.987'
+
+
+def do_test(catalog):
+    log = catalog.log
+    log.error("do_test()")
+    current_task = catalog.get_current_task_str()
+    log.error("`current_task`: '{}'".format(current_task))
+
+    # Create a Fake Entry, with some Fake Data
+    # ----------------------------------------
+    _first_source(catalog)
+
+    #
+    log_str = "ADDING SECOND SOURCE"
+    log.error("\n\n{}\n{}\n{}\n\n".format("="*100, log_str, "="*100))
+
+    # Add new Data, from different source, to same fake entry
+    # -------------------------------------------------------
+    _second_source(catalog)
+
+    return
+
+
+def _first_source(catalog):
+    """Try adding a single source, with some data.
+    """
+    log = catalog.log
+    # Add Entry to Catalog
+    log.error("Calling: ``add_entry('{}')``".format(TEST_NAME))
+    name = catalog.add_entry(TEST_NAME)
+    log.error("\t `name`: '{}'".format(name))
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    # Make sure entry exists
+    if TEST_NAME not in catalog.entries:
+        raise RuntimeError("`TEST_NAME`: '{}' is not in entries".format(
+            TEST_NAME))
+    # Make sure entry has the correct name
+    stored_name = catalog.entries[TEST_NAME][SN_KEYS.NAME]
+    if stored_name != TEST_NAME:
+        raise RuntimeError("`TEST_NAME`[{}]: '{}' does not match".format(
+            SN_KEYS.NAME, stored_name, TEST_NAME))
+
+    # Add source to entry
+    log.error("Calling: ``add_source('{}')``".format(FAKE_BIBCODE_1))
+    source = catalog.entries[name].add_source(
+        bibcode=FAKE_BIBCODE_1)
+    log.error("\t `source`: '{}'".format(source))
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    # Make sure source alias is correct
+    if source != '1':
+        raise RuntimeError("Returned `source`: '{}' is wrong.".format(source))
+    # Make sure source has the right properties
+    check_source_1(catalog, name)
+
+    # Add alias
+    log.error("Calling: ``add_quantity('alias', '{}', '{}')``".format(
+        FAKE_ALIAS_1, source))
+    catalog.entries[name].add_quantity('alias', FAKE_ALIAS_1, source)
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    # Make sure source alias is correct
+    stored_aliases = catalog.entries[name][SN_KEYS.ALIAS]
+    if ((len(stored_aliases) != 1 or
+         stored_aliases[0]['value'] != FAKE_ALIAS_1 or
+         stored_aliases[0]['source'] != source)):
+        raise RuntimeError("Stored alias: '{}' looks wrong.".format(
+            stored_aliases[0]))
+
+    log.error("Calling: ``add_quantity('redshift', '{}', '{}')``".format(
+        FAKE_REDZ_1, source))
+    catalog.entries[name].add_quantity(
+        'redshift', FAKE_REDZ_1, source, kind='spectroscopic')
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+
+    log.error("Calling: ``journal_entries()``")
+    catalog.journal_entries()
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    # Make sure the remaining stub looks right
+    check_stub(catalog, name)
+    return
+
+
+def _second_source(catalog):
+    log = catalog.log
+
+    log.error("Calling: ``add_entry('{}')``".format(FAKE_ALIAS_1))
+    name = catalog.add_entry(FAKE_ALIAS_1)
+    log.error("\t `name`: '{}'".format(name))
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    # Make sure the proper name is returned (instead of the alias)
+    if name != TEST_NAME:
+        raise RuntimeError("Returned `name`: '{}' does not match '{}'".format(
+            name, TEST_NAME))
+    # Make sure previous data was loaded
+    check_source_1(catalog, name)
+
+    log.error("Calling: ``add_source('{}')``".format(FAKE_BIBCODE_2))
+    source = catalog.entries[name].add_source(
+        bibcode=FAKE_BIBCODE_2)
+    log.error("\t `source`: '{}'".format(source))
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+
+    log.error("Calling: ``add_quantity('alias', '{}', '{}')``".format(
+        FAKE_ALIAS_2, source))
+    catalog.entries[name].add_quantity('alias', FAKE_ALIAS_2, source)
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    check_source_2(catalog, name)
+
+    log.error("Calling: ``add_quantity('redshift', '{}', '{}')``".format(
+        FAKE_REDZ_2, source))
+    catalog.entries[name].add_quantity(
+        'redshift', FAKE_REDZ_2, source, kind='spectroscopic')
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+
+    log.error("Calling: ``journal_entries()``")
+    catalog.journal_entries()
+    log.error("\n{}\n".format(repr(catalog.entries[name])))
+    check_stub(catalog, name)
+    return
+
+
+def check_source_1(catalog, name):
+    stored_sources = catalog.entries[name][SN_KEYS.SOURCES]
+    if ((len(stored_sources) != 1 or
+         stored_sources[0][SN_KEYS.NAME] != FAKE_BIBCODE_1 or
+         stored_sources[0][SN_KEYS.BIBCODE] != FAKE_BIBCODE_1)):
+        raise RuntimeError("Stored source: '{}' looks wrong.".format(
+            stored_sources[0]))
+    return
+
+
+def check_source_2(catalog, name):
+    stored_sources = catalog.entries[name][SN_KEYS.SOURCES]
+    names = [src[SN_KEYS.NAME] for src in stored_sources]
+    codes = [src[SN_KEYS.NAME] for src in stored_sources]
+    if ((len(stored_sources) != 2 or
+         FAKE_BIBCODE_1 not in names or FAKE_BIBCODE_2 not in names or
+         FAKE_BIBCODE_1 not in codes or FAKE_BIBCODE_2 not in codes)):
+        raise RuntimeError("Stored sources: '{}' look wrong.".format(
+            stored_sources))
+    return
+
+
+def check_stub(catalog, name):
+    if not catalog.entries[name]._stub:
+        raise RuntimeError("Remaining entry is not a stub.")
+    if SN_KEYS.ALIAS not in catalog.entries[name]:
+        raise RuntimeError("Remaining entry is missing '{}'.".format(
+            SN_KEYS.ALIAS))
+    if SN_KEYS.SOURCES in catalog.entries[name]:
+        raise RuntimeError("Remaining still has '{}'.".format(
+            SN_KEYS.SOURCES))
+    return
