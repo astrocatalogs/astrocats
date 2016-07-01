4f80c363bf214fcffbb002af75abd8fc0a1666c8
guillochon <guillochon@gmail.com>
2016-07-01 01:52:49 -0400

    ENH: re-added rst files.

--- docs/conf.py
+++ docs/conf.py
@@ -346,21 +346,3 @@ texinfo_documents = [
 #
 # texinfo_no_detailmenu = False
 
-
-def run_apidoc(_):
-    modules = ['../astrocats']
-    for module in modules:
-        cur_dir = os.path.abspath(os.path.dirname(__file__))
-        output_path = os.path.join(cur_dir, module, 'doc')
-        cmd_path = 'sphinx-apidoc'
-        # Check to see if we are in a virtualenv
-        if hasattr(sys, 'real_prefix'):
-            # If we are, assemble the path manually
-            cmd_path = os.path.abspath(os.path.join(
-                sys.prefix, 'bin', 'sphinx-apidoc'))
-        subprocess.check_call(
-            [cmd_path, '-e', '-o', output_path, module, '--force'])
-
-
-def setup(app):
-    app.connect('builder-inited', run_apidoc)
