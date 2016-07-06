7fe9a9fd1c9b14c4f2e34de8692604b6aaa1abae
guillochon <guillochon@gmail.com>
2016-07-06 12:17:49 +0300

    ENH: added set_preferred_names task

---
+++ astrocats/supernovae/tasks/set_preferred_names.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Set preferred supernova names.
+"""
+
+
+def set_preferred_names(catalog):
+    """This simply calls the catalog function that performs this task.
+    """
+    catalog.set_preferred_names()
+    return
