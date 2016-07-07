1d70b0d6b207471c89ca31823d80a280405e7030
guillochon <guillochon@gmail.com>
2016-07-07 12:32:36 -0400

    MAINT: updated version number.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="astrocats",
-    version="0.1.0",
+    version="0.1.1",
     author="James Guillochon",
     author_email="guillochon@gmail.com",
     description=("Package for downloading, analyzing, and constructing open "
