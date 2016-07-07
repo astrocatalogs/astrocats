6a87e7eb2405939e7b1c59412adb7a06fe474203
lzkelley <lkelley@cfa.harvard.edu>
2016-07-07 17:45:21 -0400

    ENH: make execution or import of 'astrocats' fail without a configuration file.

--- astrocats/__init__.py
+++ astrocats/__init__.py
@@ -1,6 +1,16 @@
 """Astrocats: Scripts for creating and analyzing catalogs of astronomical data.
 """
 
+import os
+
 __version__ = '0.1.7'
 __author__ = 'James Guillochon'
 __license__ = 'MIT'
+
+_CONFIG_PATH = os.path.join(os.path.expanduser('~'),
+                            '.config', 'astrocats', 'astrocatsrc')
+
+if not os.path.isfile(_CONFIG_PATH):
+    raise RuntimeError("'{}' does not exist.  "
+                       "Run `astrocats setup` to configure."
+                       "".format(_CONFIG_PATH))
