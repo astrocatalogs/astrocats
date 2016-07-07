4b1422e46a251f5e55f0f1d21b3f34a53a50abae
guillochon <guillochon@gmail.com>
2016-07-07 12:53:28 -0400

    MAINT: updated setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ reqs = [str(req.req) for req in install_reqs]
 
 setup(
     name="astrocats",
-    version="0.1.2",
+    version="0.1.3",
     author="James Guillochon",
     author_email="guillochon@gmail.com",
     description=("Package for downloading, analyzing, and constructing open "
