37c0378e0e7ac3a9d473f8870fda6d8f107593f2
guillochon <guillochon@gmail.com>
2016-07-21 15:07:48 -0400

    MAINT: Don't add `U_TIME` if `TIME` doesn't exist

--- astrocats/catalog/photometry.py
+++ astrocats/catalog/photometry.py
@@ -95,7 +95,7 @@ class Photometry(CatDict):
                         self[bmf] = temp
 
         # Time unit is necessary for maximum time determination
-        if self._KEYS.U_TIME not in self:
+        if self._KEYS.U_TIME not in self and self._KEYS.TIME in self:
             self._log.info('`{}` not found in photometry, assuming '
                            ' MJD.'.format(self._KEYS.U_TIME))
             self[self._KEYS.U_TIME] = 'MJD'
