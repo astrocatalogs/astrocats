281bab762b006915ae210c95fae16189b73434e5
lzkelley <lkelley@cfa.harvard.edu>
2016-07-25 13:57:04 -0400

    MAINT: resolve to absolute path when running git commands.

--- astrocats/catalog/catalog.py
+++ astrocats/catalog/catalog.py
@@ -193,7 +193,7 @@ class Catalog:
         catalog_sha = subprocess.check_output(git_command, cwd=my_path)
         catalog_sha = catalog_sha.decode('ascii').strip()
         # Git SHA of `astrocats`
-        parent_path = os.path.join(my_path, os.pardir)
+        parent_path = os.path.abspath(os.path.join(my_path, os.pardir))
         self.log.debug("Running '{}' in '{}'.".format(git_command, parent_path))
         astrocats_sha = subprocess.check_output(git_command, cwd=parent_path)
         astrocats_sha = astrocats_sha.decode('ascii').strip()
