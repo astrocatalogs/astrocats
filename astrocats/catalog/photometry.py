655a7f846e1e8603823c9385e4967352bae93806
guillochon <guillochon@gmail.com>
2016-09-20 20:54:08 -0400

    ENH: added `E_LOWER_FLUX` and `E_UPPER_FLUX`

--- astrocats/catalog/photometry.py
+++ astrocats/catalog/photometry.py
@@ -33,6 +33,8 @@ class PHOTOMETRY(KeyCollection):
     E_UNABSORBED_FLUX = Key('e_unabsorbedflux', KEY_TYPES.NUMERIC)
     E_LOWER_MAGNITUDE = Key('e_lower_magnitude', KEY_TYPES.NUMERIC)
     E_UPPER_MAGNITUDE = Key('e_upper_magnitude', KEY_TYPES.NUMERIC)
+    E_LOWER_FLUX = Key('e_lower_flux', KEY_TYPES.NUMERIC)
+    E_UPPER_FLUX = Key('e_upper_flux', KEY_TYPES.NUMERIC)
     E_LOWER_TIME = Key('e_lower_time', KEY_TYPES.NUMERIC)
     E_UPPER_TIME = Key('e_upper_time', KEY_TYPES.NUMERIC)
 
