817f55e7115bdffe9ea532d6171608d3ecebbcce
guillochon <guillochon@gmail.com>
2017-03-12 11:21:48 -0400

    ENH: add `sortOD` util func

--- astrocats/catalog/utils/sorting.py
+++ astrocats/catalog/utils/sorting.py
@@ -1,9 +1,11 @@
 '''Key sorting functions
 '''
 
+from collections import OrderedDict
+
 from .digits import is_integer
 
-__all__ = ['alias_priority', 'bib_priority', 'repo_priority']
+__all__ = ['alias_priority', 'bib_priority', 'repo_priority', 'sortOD']
 
 
 def alias_priority(name, attr):
@@ -16,7 +18,7 @@ def bib_priority(attr):
     if attr.get('secondary', False):
         if 'bibcode' in attr:
             if is_integer(attr['bibcode'][:4]):
-                return (3000-int(attr['bibcode'][:4]), '')
+                return (3000 - int(attr['bibcode'][:4]), '')
         if 'name' in attr:
             return (0, attr['name'])
         return (0, '')
@@ -31,3 +33,13 @@ def repo_priority(attr):
     if is_integer(attr[-4:]):
         return int(attr[-4:])
     return 1000000000
+
+
+def sortOD(od):
+    res = OrderedDict()
+    for k, v in sorted(od.items()):
+        if isinstance(v, dict):
+            res[k] = sortOD(v)
+        else:
+            res[k] = v
+    return res
