f205d27640d8809220e1cb37d4b64eadaf577e8f
lzkelley <lkelley@cfa.harvard.edu>
2017-05-29 17:16:54 -0400

    ENH: add 'production/templates' for HTML file templates

--- astrocats/catalog/production/__init__.py
+++ astrocats/catalog/production/__init__.py
@@ -1,6 +1,10 @@
 """Astrocats: Production submodule.
 """
 # flake8: noqa  --- ignore imported but unused flake8 warnings
+import os
+
+_DIR_THIS = os.path.realpath(os.path.dirname(__file__))
+DIR_TEMPLATES = os.path.join(_DIR_THIS, "templates", "")
 
 from . import director
 from . import producer
