2612591c20c0bd251684cdc82fb018fc617f6f68
lzkelley <lkelley@cfa.harvard.edu>
2017-05-30 12:39:31 -0400

    ENH: added failed and missing sdss images

--- astrocats/catalog/production/__init__.py
+++ astrocats/catalog/production/__init__.py
@@ -6,6 +6,9 @@ import os
 _DIR_THIS = os.path.realpath(os.path.dirname(__file__))
 DIR_TEMPLATES = os.path.join(_DIR_THIS, "templates", "")
 
+PATH_SDSS_MISSING_HOST_IMAGE = os.path.join(DIR_TEMPLATES, "sdss_missing_host.jpg")
+PATH_SDSS_FAILED_HOST_IMAGE = os.path.join(DIR_TEMPLATES, "sdss_failed_host.jpg")
+
 from . import director
 from . import producer
 from . import utils
