ccefdb90ea558c5e3a1ca81fa65647e78e891583
lzkelley <lkelley@cfa.harvard.edu>
2017-05-30 16:32:07 -0400

    MAINT: import some frequent KeyCollections in 'catalog/__init__.py' so they can be accessed more easily.

--- astrocats/catalog/__init__.py
+++ astrocats/catalog/__init__.py
@@ -3,4 +3,9 @@
 # flake8: noqa  --- ignore imported but unused flake8 warnings
 
 from . import main
+
+from . entry import ENTRY
+from . quantity import QUANTITY
+from . source import SOURCE
+
 from . import catalog
