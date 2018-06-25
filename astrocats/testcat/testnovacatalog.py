2bc4fbb54ed7fb0a44335d3bcfee76c3650a57c8
lzkelley <lkelley@cfa.harvard.edu>
2018-06-25 19:35:32 -0400

    Use 'PATHS.PATH_CACHE' variable

--- astrocats/testcat/testnovacatalog.py
+++ astrocats/testcat/testnovacatalog.py
@@ -26,25 +26,18 @@ class TestnovaCatalog(Catalog):
             """Initialize paths."""
             super(TestnovaCatalog.PATHS, self).__init__(catalog)
             # auxiliary datafiles
-            self.TYPE_SYNONYMS = os.path.join(
-                self.PATH_INPUT, 'type-synonyms.json')
-            self.SOURCE_SYNONYMS = os.path.join(
-                self.PATH_INPUT, 'source-synonyms.json')
-            self.URL_REDIRECTS = os.path.join(
-                self.PATH_INPUT, 'url-redirects.json')
-            self.NON_SNE_TYPES = os.path.join(
-                self.PATH_INPUT, 'non-sne-types.json')
-            self.NON_SNE_PREFIXES = os.path.join(
-                self.PATH_INPUT, 'non-sne-prefixes.json')
+            self.TYPE_SYNONYMS = os.path.join(self.PATH_INPUT, 'type-synonyms.json')
+            self.SOURCE_SYNONYMS = os.path.join(self.PATH_INPUT, 'source-synonyms.json')
+            self.URL_REDIRECTS = os.path.join(self.PATH_INPUT, 'url-redirects.json')
+            self.NON_SNE_TYPES = os.path.join(self.PATH_INPUT, 'non-sne-types.json')
+            self.NON_SNE_PREFIXES = os.path.join(self.PATH_INPUT, 'non-sne-prefixes.json')
             self.BIBERRORS = os.path.join(self.PATH_INPUT, 'biberrors.json')
             self.ATELS = os.path.join(self.PATH_INPUT, 'atels.json')
             self.CBETS = os.path.join(self.PATH_INPUT, 'cbets.json')
             self.IAUCS = os.path.join(self.PATH_INPUT, 'iaucs.json')
             # cached datafiles
-            self.BIBAUTHORS = os.path.join(
-                self.PATH_OUTPUT, 'cache', 'bibauthors.json')
-            self.EXTINCT = os.path.join(
-                self.PATH_OUTPUT, 'cache', 'extinctions.json')
+            self.BIBAUTHORS = os.path.join(self.PATH_CACHE, 'bibauthors.json')
+            self.EXTINCT = os.path.join(self.PATH_CACHE, 'extinctions.json')
 
         # def get_repo_years(self):
         #     """Return an array of years based upon output repositories."""
