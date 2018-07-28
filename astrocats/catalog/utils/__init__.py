a7b325f1b728a2b398afd005c05306cbed693474
lzkelley <lkelley@cfa.harvard.edu>
2018-07-28 18:24:29 -0400

    Add em-bands functions and variables that were stored in 'photometry.py' previously

--- astrocats/catalog/utils/__init__.py
+++ astrocats/catalog/utils/__init__.py
@@ -1,7 +1,7 @@
 """General utility functions used by multiple OSC scripts.
 """
 
-from . import (dates, digits, imports, logger, plotting, sorting, strings,
+from . import (dates, digits, em_bands, imports, logger, plotting, sorting, strings,
                tq_funcs, lists)
 from .dates import *
 from .digits import *
@@ -12,10 +12,12 @@ from .plotting import *
 from .sorting import *
 from .strings import *
 from .tq_funcs import *
+from .em_bands import *
 
 __all__ = []
 __all__.extend(dates.__all__)
 __all__.extend(digits.__all__)
+__all__.extend(em_bands.__all__)
 __all__.extend(imports.__all__)
 __all__.extend(lists.__all__)
 __all__.extend(logger.__all__)
