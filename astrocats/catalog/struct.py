ea62f7d3ce8bae093c492ac133cb2e90fe159495
lzkelley <lkelley@cfa.harvard.edu>
2018-07-29 10:40:06 -0400

    BUG,MAINT: incorrect log variable reference, some cleanup

--- astrocats/catalog/struct.py
+++ astrocats/catalog/struct.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
-from astrocats.catalog.catdict import CatDictError
-from astrocats.catalog.key import KEY_TYPES, Key
-from astrocats.catalog.utils import (astrotime, is_number, listify, uniq_cdl,
-                                     bandmetaf, bandrepf, instrumentrepf)
+import astrocats
+# from astrocats.catalog.catdict import CatDictError
+# from astrocats.catalog.key import KEY_TYPES, Key
+# from astrocats.catalog.utils import (astrotime, listify, uniq_cdl,
+#                                      bandmetaf, bandrepf, instrumentrepf)
+from astrocats.catalog import utils
 
 _PAS_PATH = "/Users/lzkelley/Research/catalogs/astroschema"
 if _PAS_PATH not in sys.path:
@@ -33,7 +35,7 @@ class My_Meta_Struct(pas.struct.Meta_Struct):
         other_aliases = other[self._KEYS.SOURCE].split(',')
 
         # Store alias to `self`
-        self[self._KEYS.SOURCE] = uniq_cdl(self_aliases + other_aliases)
+        self[self._KEYS.SOURCE] = utils.uniq_cdl(self_aliases + other_aliases)
 
         return
 
@@ -99,8 +101,9 @@ class _Quantity(My_Meta_Struct):
                     aliases = [value]
 
                 if df[self._KEYS.VALUE] in aliases:
-                    raise CatDictError("Alias '{}' in '{}'\' '{}' list".format(
-                        value, parent[parent._KEYS.NAME], parent._KEYS.DISTINCT_FROM))
+                    err = "Alias '{}' in '{}'\' '{}' list".format(
+                        value, parent[parent._KEYS.NAME], parent._KEYS.DISTINCT_FROM)
+                    raise astrocats.catalog.catdict.CatDictError(err)
 
             if value in parent.catalog.entries:
                 for df in parent.catalog.entries[value].get(
@@ -108,26 +111,28 @@ class _Quantity(My_Meta_Struct):
                     if df[self._KEYS.VALUE] in parent.get_aliases():
                         err = "Alias '{}' in '{}'\' '{}' list".format(
                             value, parent[parent._KEYS.NAME], parent._KEYS.DISTINCT_FROM)
-                        raise CatDictError(err)
+                        raise astrocats.catalog.catdict.CatDictError(err)
 
         # Check that value exists
         if (not self[self._KEYS.VALUE] or self[self._KEYS.VALUE] == '--' or
                 self[self._KEYS.VALUE] == '-'):
             err = "Value '{}' is empty, not adding to '{}'".format(
                 self[self._KEYS.VALUE], parent[parent._KEYS.NAME])
-            raise CatDictError(err)
+            raise astrocats.catalog.catdict.CatDictError(err)
 
         if not parent._clean_quantity(self):
             err = "Value '{}' did not survive cleaning process, not adding to '{}'.".format(
                 self[self._KEYS.VALUE], parent[parent._KEYS.NAME])
-            raise CatDictError(err)
+            raise astrocats.catalog.catdict.CatDictError(err)
 
         # Check that quantity value matches type after cleaning
+        '''
         if isinstance(self._key, Key):
             if (self._key.type == KEY_TYPES.NUMERIC) and (not is_number(self[self._KEYS.VALUE])):
                 err = "Value '{}' is not numeric, not adding to '{}'".format(
                     self[self._KEYS.VALUE], parent[parent._KEYS.NAME])
-                raise CatDictError(err)
+                raise astrocats.catalog.catdict.CatDictError(err)
+        '''
 
         # print("re-Validating...")
         self.validate()
@@ -162,20 +167,20 @@ class _Photometry(My_Meta_Struct):
 
             for bmf in bandmetaf_keys:
                 if bmf not in self:
-                    temp = bandmetaf(sband, bmf)
+                    temp = utils.bandmetaf(sband, bmf)
                     if temp is not None:
                         self[bmf] = temp
 
         # Convert dates to MJD
-        timestrs = [str(x) for x in listify(self.get(self._KEYS.TIME, ''))]
+        timestrs = [str(x) for x in utils.listify(self.get(self._KEYS.TIME, ''))]
         for ti, timestr in enumerate(timestrs):
             if (any(x in timestr for x in ['-', '/']) and not timestr.startswith('-')):
                 timestrs[ti] = timestr.replace('/', '-')
                 try:
-                    # timestrs[ti] = str(astrotime(timestrs[ti], format='isot').mjd)
-                    timestrs[ti] = str(astrotime(timestrs[ti], input='isot', output='mjd'))
+                    # timestrs[ti] = str(utils.astrotime(timestrs[ti], format='isot').mjd)
+                    timestrs[ti] = str(utils.astrotime(timestrs[ti], input='isot', output='mjd'))
                 except Exception:
-                    raise CatDictError('Unable to convert date to MJD.')
+                    raise astrocats.catalog.catdict.CatDictError('Unable to convert date to MJD.')
             elif timestr:  # Make sure time is string
                 timestrs[ti] = timestr
 
@@ -184,14 +189,14 @@ class _Photometry(My_Meta_Struct):
 
         # Time unit is necessary for maximum time determination
         if self._KEYS.U_TIME not in self and self._KEYS.TIME in self:
-            self._log.info('`{}` not found in photometry, assuming '
-                           ' MJD.'.format(self._KEYS.U_TIME))
+            msg = '`{}` not found in photometry, assuming MJD.'.format(self._KEYS.U_TIME)
+            self._parent._log.info(msg)
             self[self._KEYS.U_TIME] = 'MJD'
 
         if (self._KEYS.U_COUNT_RATE not in self and
                 self._KEYS.COUNT_RATE in self):
-            self._log.info('`{}` not found in photometry, assuming '
-                           ' s^-1.'.format(self._KEYS.U_COUNT_RATE))
+            msg = '`{}` not found in photometry, assuming s^-1.'.format(self._KEYS.U_COUNT_RATE)
+            self._parent._log.info(msg)
             self[self._KEYS.U_COUNT_RATE] = 's^-1'
 
         return
@@ -201,9 +206,9 @@ class _Photometry(My_Meta_Struct):
 
         # Do some basic homogenization
         if key == self._KEYS.BAND:
-            return bandrepf(value)
+            return utils.bandrepf(value)
         elif key == self._KEYS.INSTRUMENT:
-            return instrumentrepf(value)
+            return utils.instrumentrepf(value)
 
         return value
 
