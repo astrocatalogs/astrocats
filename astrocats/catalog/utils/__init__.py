aa025e628f1bcdfd205b08ca45b6ce58b7fca387
lzkelley <lkelley@cfa.harvard.edu>
2018-07-29 21:25:54 -0400

    Dont import 'em_bands', causing circular imports, should be moved to supernovae

--- astrocats/catalog/utils/__init__.py
+++ astrocats/catalog/utils/__init__.py
@@ -1,7 +1,9 @@
 """General utility functions used by multiple OSC scripts.
 """
 
-from . import (dates, digits, em_bands, imports, logger, plotting, sorting, strings,
+from . import (dates, digits,
+               # em_bands,
+               imports, logger, plotting, sorting, strings,
                tq_funcs, lists)
 from .dates import *
 from .digits import *
@@ -12,12 +14,12 @@ from .plotting import *
 from .sorting import *
 from .strings import *
 from .tq_funcs import *
-from .em_bands import *
+# from .em_bands import *
 
 __all__ = []
 __all__.extend(dates.__all__)
 __all__.extend(digits.__all__)
-__all__.extend(em_bands.__all__)
+# __all__.extend(em_bands.__all__)
 __all__.extend(imports.__all__)
 __all__.extend(lists.__all__)
 __all__.extend(logger.__all__)
