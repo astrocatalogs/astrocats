fb546584c5c8b79a7cb8fd8837bb02453fc5de42
lzkelley <lkelley@cfa.harvard.edu>
2018-08-12 12:58:34 -0400

    Add 'pyastroschema' path to 'sys.path' for now; store git version as '__git_version__'

--- astrocats/__init__.py
+++ astrocats/__init__.py
@@ -3,20 +3,26 @@
 Scripts for creating and analyzing catalogs of astronomical data.
 """
 
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
 import os
+import sys
 
 __version__ = '0.3.42'
 __author__ = 'James Guillochon & Luke Kelley'
 __license__ = 'MIT'
 
-# Set the path for the user's configuration file
-_CONFIG_PATH = os.path.join(
-    os.path.expanduser('~'), '.config', 'astrocats', 'astrocatsrc')
-
 _PATH_ROOT = os.path.join(os.path.dirname(__file__), "")
 _PATH_CATALOG = os.path.join(_PATH_ROOT, "catalog", "")
 _PATH_SCHEMA = os.path.join(_PATH_ROOT, "schema", "")
 
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+warnings.warn("Adding `pyastroschema` to `sys.path` for easy access... fix this")
+_PAS_PATH = "/Users/lzkelley/Research/catalogs/astroschema"
+if _PAS_PATH not in sys.path:
+    sys.path.append(_PAS_PATH)
+
+from .utils import gitter
+
+__git_version__ = gitter.get_git()
