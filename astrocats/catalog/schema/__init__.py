df52c5faa2c642532bf0c76f3aa61e5c5604488f
lzkelley <lkelley@cfa.harvard.edu>
2018-08-13 12:00:56 -0400

    Move files to input then to output

--- astrocats/catalog/schema/__init__.py
+++ astrocats/catalog/schema/__init__.py
@@ -2,22 +2,18 @@
 """
 
 import os
-import sys
+# import sys
 import warnings
 import shutil
+import glob
 from collections import OrderedDict
 
 import astrocats
-from astrocats.catalog import struct
-
-_PAS_PATH = "/Users/lzkelley/Research/catalogs/astroschema"
-if _PAS_PATH not in sys.path:
-    sys.path.append(_PAS_PATH)
+from astrocats import PATHS as AC_PATHS
+# from astrocats.catalog import struct
 
 import pyastroschema as pas  # noqa
 
-PATH_SCHEMA_INPUT = os.path.join(astrocats._PATH_SCHEMA, "input", "")
-PATH_SCHEMA_OUTPUT = os.path.join(astrocats._PATH_SCHEMA, "output", "")
 
 # This describes replacements/renames that should be applied to the file corresponding to the
 # given schema name
@@ -32,15 +28,73 @@ _REPLACE = {
 }
 
 
-def main(add_files=[], add_structures=[]):
+def setup(catalog=None, log=None):
+    if catalog is not None:
+        log = catalog.log
+    elif (log is None):
+        raise RuntimeError("If no `catalog` is provided, `log` is required")
+
+    # Clear output directory
+    pattern = os.path.join(AC_PATHS.SCHEMA_OUTPUT, "", "*.json")
+    old_files = sorted(glob.glob(pattern))
+    old_files = [os.path.join(AC_PATHS.SCHEMA_OUTPUT, fname) for fname in old_files]
+    log.info("Deleting {} old schema files.".format(len(old_files)))
+    for fname in old_files:
+        os.remove(fname)
 
     # Copy all `astroschema` schema files to the 'input' directory
-    schema_fnames = pas.copy_schema_files(PATH_SCHEMA_INPUT, verbose=True)
-    for src in add_files:
-        dst = os.path.basename(src)
-        dst = os.path.join(PATH_SCHEMA_INPUT, dst)
+    log.info("Copying `astroschema` schema files to input")
+    astroschema_fnames = pas.copy_schema_files(AC_PATHS.SCHEMA_INPUT_ASTROSCHEMA, verbose=False)
+    for fname in astroschema_fnames:
+        base = os.path.basename(fname)
+        log.debug("Copied '{}' to '{}'".format(base, fname))
+
+    log.info("Copied {} files".format(len(astroschema_fnames)))
+
+    # Load names of astrocats files (already in 'input')
+    log.info("Loading `astrocats` schema file names from input")
+    pattern = os.path.join(AC_PATHS.SCHEMA_INPUT_ASTROCATS, "*.json")
+    astrocats_fnames = list(sorted(glob.glob(pattern)))
+    # Add directory path
+    pattern_path = os.path.dirname(pattern)
+    astrocats_fnames = [os.path.join(pattern_path, fname) for fname in astrocats_fnames]
+
+    log.info("Loaded {} file names".format(len(astrocats_fnames)))
+
+    def get_output_fname(src, group):
+        base = os.path.basename(src)
+        if not base.lower().startswith(group.lower()):
+            base = group.lower() + "_" + base
+        dst = os.path.join(AC_PATHS.SCHEMA_OUTPUT, base)
+        return dst
+
+    # Store all source filenames from input and construct the destination names for output
+    src_list = []
+    dst_list = []
+    fnames = [astroschema_fnames, astrocats_fnames]
+    groups = ["astroschema", "astrocats"]
+    log.info("Copying `astrocats` schema files to input")
+    for src_files, grp in zip(fnames, groups):
+        for src in src_files:
+            src_list.append(src)
+            dst = get_output_fname(src, grp)
+            # Make sure there are no duplicates
+            if dst in dst_list:
+                idx = dst_list.index(dst)
+                err = "Destination '{}' already exists for '{}', cannot be used for '{}'!".format(
+                    dst, src_list[idx], src)
+                log.raise_error(err, ValueError)
+
+            dst_list.append(dst)
+
+    # Copy all input schema files to output with modified names
+    log.info("Copying {} schema files to output".format(len(src_list)))
+    for src, dst in zip(src_list, dst_list):
         shutil.copy(src, dst)
-        # print("Copied '{}' ==> '{}'".format(src, dst))
+        base = os.path.basename(dst)
+        log.debug("Copied '{}' : '{}' ==> '{}'".format(base, src, dst))
+
+    return
 
     mod_files = schema_fnames + add_files
 
