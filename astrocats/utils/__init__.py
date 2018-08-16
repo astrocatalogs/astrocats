97f360b2986faa1906b2c7690bd51ac0a5d23731
lzkelley <lkelley@cfa.harvard.edu>
2018-08-16 10:20:28 -0400

    BUG: fix import error

--- astrocats/utils/__init__.py
+++ astrocats/utils/__init__.py
@@ -2,13 +2,12 @@
 """
 # flake8: noqa  --- ignore imported but unused flake8 warnings
 
-from . import (dates, digits, imports, logger, plotting, sorting, strings, tq_funcs)
+from . import (dates, digits, imports, logger, sorting, strings, tq_funcs)
 from .dates import *
 from .digits import *
 from .imports import *
 from .lists import *
 from .logger import *
-# from .plotting import *
 from .sorting import *
 from .strings import *
 from .tq_funcs import *
@@ -17,11 +16,9 @@ from .math import *
 __all__ = []
 __all__.extend(dates.__all__)
 __all__.extend(digits.__all__)
-# __all__.extend(em_bands.__all__)
 __all__.extend(imports.__all__)
 __all__.extend(lists.__all__)
 __all__.extend(logger.__all__)
-# __all__.extend(plotting.__all__)
 __all__.extend(sorting.__all__)
 __all__.extend(strings.__all__)
 __all__.extend(tq_funcs.__all__)
